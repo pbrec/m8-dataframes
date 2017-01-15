e0254431dc9f2123286859ff2ccba062eea5578e
pbrec <pbrand@ucdavis.edu>
2017-01-14 17:55:48 -0800

    added answers to ex1

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,42 +1,46 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
 
+emp<-sapply(1:200, function(x) paste("Employee", x))
 
 # Create a vector of 2014 salaries using the runif function
 
+sal14<-runif(200,1600,2200)
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+sal15<-runif(200,1700,2300)
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
-
+salaries<-data.frame(sal14,sal15)
+rownames(salaries)<-emp
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise<-salaries$sal15-salaries$sal14
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise<-salaries$raise>0
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+salaries['Employee 57','sal15']
 
 # How many employees got a raise?
-
+length(which(salaries$got.raise) )
 
 # What was the value of the highest raise?
-
+max(salaries$raise)
 
 # What was the name of the employee who recieved the highest raise?
-
+rownames(salaries[salaries$raise==max(salaries$raise),])
 
 # What was the largest decrease in salaries between the two years?
-
+min(salaries$raise)
 
 # What was the name of the employee who recieved largest decrease in salary?
-
+rownames(salaries[salaries$raise==min(salaries$raise),])
 
 # What was the average salary increase?
+
+mean(salaries$raise[salaries$got.raise==T])
 
 
 ### Bonus ###
@@ -45,7 +49,7 @@
 
 
 # For people who did not get a raise, how much money did they lose?
-
+mean(salaries$raise[salaries$got.raise==F])
 
 # Is that what you expected them to lose?
 
